577687942dde5b3e36827da5d6b57e1ea4c6cf58
elton-datasc <elton-guilherme@hotmail.com>
2022-10-28 10:52:57 -0300

    "API Build"

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ app = FastAPI()
 
 galera = {
 
-1:{"nome":"fulano","signo":"libra", "cidade":"guarabira","tipo":"ansioso"},
-2:{"nome":"cirrana","signo":"capricórnio","cidade":"sjdc","tipo":"brincalhão"},
-3:{"nome":"beltrana","signo":"escorpião","cidade": "rj","tipo":"invocada"},
+1:{"produto":"refrigerante 2l","un":"garrafa", "area":"padaria","tipo":"bebida"},
+2:{"produto":"refrigerante lata","un":"lt","area":"padaria","tipo":"bebida"},
+3:{"produto":"pão de caixa","un":"un","area": "padaria","tipo":"pao"},
+4:{"produto":"catchup","un":"bisnaga", "area":"condimentos","tipo":""},
+5:{"produto":"mostarda","un":"bisnaga","area":"condimentos","tipo":""},
+6:{"produto":"maionese","un":"bisnaga","area": "condimentos","tipo":""},
+7:{"produto":"tomate","un":"kg", "area":"hortifruti","tipo":"legume"},
+8:{"produto":"laranja","un":"kg","area":"hortifruti","tipo":"fruta"},
+9:{"produto":"banana","un":"kg","area": "hortifruti","tipo":"fruta"},
 
 }
 
