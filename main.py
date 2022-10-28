9ebc37e487359ad70ac8e40605824642b4ba33dc
Elton <elton-guilherme@hotmail.com>
2022-10-28 00:28:05 -0300

    "API Build"

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+galera = {
+
+1:{"nome":"elton","signo":"libra", "cidade":"joão pessoa","tipo":"ansioso"},
+2:{"nome":"lu","signo":"capricórnio","cidade":"sjdc","tipo":"radical livre"},
+3:{"nome":"raye","signo":"escorpião","cidade": "rio de janeiro","tipo":"teimosinha"},
+
+}
+
+#criando as rotas
+
+@app.get("/galera")
+def pessoas():
+  return galera
+
+
+@app.get("/galera/{id}")
+def filtro_pessoas(id: int):
+  return galera[id]
+  
+
+@app.get("/")
+def contagem_de_pessoas():
+  return {"galera" : len(galera)}
+
