cedbfb41afbbe0eab4579b4a6a8f7e22dd7d8f15
elton-datasc <elton-guilherme@hotmail.com>
2022-10-28 15:06:10 -0300

    "API pu values"

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ app = FastAPI()
 
 vendas = {
 
-1:{"produto":"refrigerante 2l","un":"garrafa", "area":"padaria","tipo":"bebida"},
-2:{"produto":"refrigerante lata","un":"lt","area":"padaria","tipo":"bebida"},
-3:{"produto":"pão de caixa","un":"un","area": "padaria","tipo":"pao"},
-4:{"produto":"catchup","un":"bisnaga", "area":"condimentos","tipo":""},
-5:{"produto":"mostarda","un":"bisnaga","area":"condimentos","tipo":""},
-6:{"produto":"maionese","un":"bisnaga","area": "condimentos","tipo":""},
-7:{"produto":"tomate","un":"kg", "area":"hortifruti","tipo":"legume"},
-8:{"produto":"laranja","un":"kg","area":"hortifruti","tipo":"fruta"},
-9:{"produto":"banana","un":"kg","area": "hortifruti","tipo":"fruta"},
+1:{"produto":"refrigerante 2l","un":"garrafa", "area":"padaria","tipo":"bebida","pu":3.8},
+2:{"produto":"refrigerante lata","un":"lt","area":"padaria","tipo":"bebida", "pu":1.5},
+3:{"produto":"pão de caixa","un":"un","area": "padaria","tipo":"pao","pu":2.},
+4:{"produto":"catchup","un":"bisnaga", "area":"condimentos","tipo":"","pu":3.},
+5:{"produto":"mostarda","un":"bisnaga","area":"condimentos","tipo":"","pu":1.6},
+6:{"produto":"maionese","un":"bisnaga","area": "condimentos","tipo":"","pu":2.5},
+7:{"produto":"tomate","un":"kg", "area":"hortifruti","tipo":"legume","pu":.8},
+8:{"produto":"laranja","un":"kg","area":"hortifruti","tipo":"fruta","pu":1.1},
+9:{"produto":"banana","un":"kg","area": "hortifruti","tipo":"fruta","pu":3.8},
 
 }
 
